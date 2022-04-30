13e3a391ee5e53b676a166e2caf9cae83b948656
Arash <53490252+ThePythonist@users.noreply.github.com>
2022-04-30 20:33:25 +0430

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,5 @@
+c = int(input("Damaye santi gerad ra vared konid : "))
+
+f = c * (9/5) + 32
+
+print("Damaye farenheit :",f)
---
+++ 8.py
@@ -0,0 +1,8 @@
+zel1 = int(input("Zel kochak ra vared konid : "))
+zel2 = int(input("Zel bozorg ra vared konid : "))
+
+mohit = (zel1*2) + (zel2*2)
+masahat = zel1 * zel2
+
+print(mohit)
+print(masahat)
